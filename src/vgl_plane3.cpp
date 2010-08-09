2cad1c6b9bd2c46dc209e99c3f1fee5c826dfef1
Vilya Harvey <vilya.harvey@gmail.com>
2010-08-09 23:39:23 +0100

    Turned the Plane3 class into a template.

--- src/vgl_plane3.cpp
+++ src/vgl_plane3.cpp
@@ -3,15 +3,5 @@
 namespace vgl {
 
 
-//
-// Plane3 FUNCTIONS
-//
-
-Vec3f planePos(const Plane3& p, float u, float v)
-{
-  return p.corner + u * p.u + v * p.v;
-}
-
-
 } // namespace vgl
 
--- src/vgl_plane3.h
+++ src/vgl_plane3.h
@@ -9,20 +9,30 @@ namespace vgl {
 // TYPES
 //
 
+template <typename Num>
 struct Plane3 {
-  Vec3f corner;
-  Vec3f u, v;
+  Vec3<Num> corner;
+  Vec3<Num> u, v;
 
   Plane3() : corner(), u(1, 0, 0), v(0, 1, 0) {}
-  Plane3(Vec3f iCorner, Vec3f iU, Vec3f iV) : corner(iCorner), u(iU), v(iV) {}
+  Plane3(Vec3<Num> iCorner, Vec3<Num> iU, Vec3<Num> iV) : corner(iCorner), u(iU), v(iV) {}
 };
+
+
+typedef Plane3<float> Plane3f;
+typedef Plane3<double> Plane3d;
 
 
 //
 // FUNCTIONS
 //
 
-Vec3f planePos(const Plane3& p, float u, float v);
+template <typename Num>
+Vec3<Num> planePos(const Plane3<Num>& p, Num u, Num v)
+{
+  return p.corner + u * p.u + v * p.v;
+}
+
 
 } // namespace vgl
 
