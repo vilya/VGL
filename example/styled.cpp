11b403002bdb398acb1b986dd1aa19991d8f97a3
Vilya Harvey <vilya.harvey@gmail.com>
2010-08-19 01:46:42 +0100

    Modified the other examples to use the default camera instead of providing their own.
    Where appropriate, of course.
    
    The arcball example still explicitly provides a camera, even though it's actually the same as the default camera we'd get. But the point of that example is to show how to create and use an ArcballCamera, so it seemed counter-productive to take it out...
    
    Naturally the imageview example still uses an OrthoCamera, too.

--- example/styled.cpp
+++ example/styled.cpp
@@ -50,10 +50,7 @@ private:
 int main(int argc, char** argv)
 {
   ExampleRenderer renderer;
-  vgl::BaseCamera camera(
-      vgl::Vec3f(0, 0, 5), vgl::Vec3f(0, 0, 0), vgl::Vec3f(0, 1, 0),
-      -1, 1, -1, 1, 30, 800, 600);
-  vgl::Viewer viewer("Example Viewer", 800, 600, &renderer, &camera);
+  vgl::Viewer viewer("Example Viewer", 800, 600, &renderer);
   viewer.run();
 }
 
